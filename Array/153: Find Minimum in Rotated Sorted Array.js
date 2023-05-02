5a48ce67024660566bfc7b9b105152480ea5edef
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-05-01 20:54:47 -0400

    11: Container With Most Water

--- Array/153: Find Minimum in Rotated Sorted Array.js
+++ Array/153: Find Minimum in Rotated Sorted Array.js
@@ -1,5 +1,9 @@
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 // Difficulty: Medium
+// tags: binary search, two pointers
+
+// Solution
+// O(log n) time and O(1) space
 
 // There are two portions to the rotated sorted array. The left and right portion, separated by the rotation point. All numbers in the left portion are bigger than all numbers in the right portion. If we pick the middle number, m, we are either in the left or the right portion. If we are in the left portion, m will be greater than the rightmost number. If we are in the right portion, m will be smaller than the rightmost number. We move to the relevant portion based on these results, until we find the pivot point which is the minimum.
 // The left and right pointers indicate the current bounds of valid numbers we are still searching. Therefore, if l===r, there is only one potential number left, and we can return that number. If we need to move to the right, that means m was in the left portion. So l=m+1, because we don't even need to consider m. But if we move to the left, that means m was in the right portion, which is correct, so we still need to consider m, therefore r=m.
