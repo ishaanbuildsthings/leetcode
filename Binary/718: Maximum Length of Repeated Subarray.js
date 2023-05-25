469bf70331e7b9917accda7f9b62d20c3ee3cc14
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-05-25 18:01:13 -0400

    update 718

--- Binary/718: Maximum Length of Repeated Subarray.js
+++ Binary/718: Maximum Length of Repeated Subarray.js
@@ -1,17 +1,107 @@
-// computes (10^x) % C without any int overflow, using mod math
-function modPow(exponent) {
-  let startingPoint = 1;
-  for (let i = 2; i <= exponent + 1; i++) {
-    // (1*10) % MOD = ((1 % MOD) * (10 % MOD)) % MOD
-    const first = startingPoint % MOD;
-    // const second = 10 % MOD;
-    const big = (first * 10) % MOD;
-    startingPoint = big;
-  }
-  return startingPoint;
-}
+// https://leetcode.com/problems/maximum-length-of-repeated-subarray/description/
+// Difficulty: Medium
+// tags: binary search, sliding window fixed, rolling hash, beats 100%
+
+// Solution, O(log(min(n,m)) * (n+m)) time and O(n+m) space
+// I wrote 2 solutions, 1 with clean code and function re-use, and one that is more expanded (that also includes naive checks).
+/*
+This is my first solution ever to beat 100% of solutions! To find the longest length for a shared subarray, we can do a binary search on the length. The bounds of the search range are 0 and the length of the shorter subarray (since the answer can never be larger than that).
+At any given length, compute a rolling hash for the two windows, crosscheck the hashes, then to an airtight naive check. It takes n+m time to get the hashes for both n and m, and requires n+m space. If we have a collision, do the naive check which is time k, where k is the length we are checking. In expected runtime we will get log(min(n,m) * (n+m)) assuming few hash collisions. If we find a collision we search an even larger size, if not we check a smaller size. To do a naive check, we can look up the hashkey in an object that maps the hashkey to the relevant coordinates of the subarray.
+*/
+// * Solution 3- there is also an n*(m+n) solution where we slide the smaller subarray over the larger one. As we slde it, we iterate over the shared portion. As we see matches we increment a count, if we see a mismatch we reset the count. I haven't implemented this to see the finer details, but the rough idea does work.
+
+// Solution 1, clean code and function re-use
 
 const MOD = 10 ** 9 + 7;
+
+var findLength = function (nums1, nums2) {
+  let l = 0;
+  let r = Math.min(nums1.length, nums2.length);
+  while (l <= r) {
+    const m = Math.floor((r + l) / 2); // try a size of m
+    const nums1Hashes = rollingHash(nums1, m);
+    const nums2Hashes = rollingHash(nums2, m);
+    let matchFound = false;
+    for (const hash of Array.from(nums1Hashes)) {
+      if (nums2Hashes.has(hash)) {
+        l = m + 1;
+        matchFound = true;
+        break;
+      }
+    }
+    if (!matchFound) {
+      r = m - 1;
+    }
+  }
+  return r;
+};
+
+// returns 101^e % MOD
+const cache = {};
+function modPow(e, base = 101) {
+  // fails if we try multiple bases
+  if (e in cache) {
+    return cache[e];
+  }
+  let current = 1;
+  for (let i = 1; i <= e; i++) {
+    // (a * b) % c = ((a % c) * (b % c)) % C
+    // (a * 10) % c = ((a % c) * 10) % c
+    const left = current % MOD;
+    current = (left * base) % MOD;
+  }
+  cache[e] = current;
+  return current;
+}
+
+// returns a set of all hashes over an arr with windows of given length
+function rollingHash(arr, length) {
+  const hashes = new Set();
+  const base = 101;
+  let currentHash = 0;
+  // build initial window
+  for (let i = 0; i < length; i++) {
+    const power = length - i - 1;
+    const number = arr[i];
+    const contribution = (number * modPow(power)) % MOD;
+    currentHash = (currentHash + contribution) % MOD;
+  }
+  hashes.add(currentHash);
+
+  // slide over remaining array
+  for (let left = 1; left <= arr.length - length; left++) {
+    const lostNumber = arr[left - 1];
+    const lostNumberPower = modPow(length - 1);
+    const lostNumberContribution = (lostNumber * lostNumberPower) % MOD;
+    currentHash = currentHash - lostNumberContribution;
+    if (currentHash < 0) currentHash += MOD;
+    currentHash = (currentHash * base) % MOD;
+    currentHash = (currentHash + arr[left + length - 1]) % MOD;
+    hashes.add(currentHash);
+  }
+
+  return hashes;
+}
+
+// Solution 2, more expanded, includes naive checks
+
+const cache2 = {}; // maps exponent to result
+// computes 101^exponent % MOD
+function modPow(exponent) {
+  if (exponent in cache) return cache[exponent];
+
+  let startingPoint = 1;
+  for (let i = 2; i <= exponent + 1; i++) {
+    // (1*101) % MOD = ((1 % MOD) * (101 % MOD)) % MOD
+    const first = startingPoint % MOD;
+    const second = 101 % MOD;
+    const big = (first * second) % MOD;
+    startingPoint = big;
+  }
+  cache[exponent] = startingPoint;
+  return startingPoint;
+}
+const MOD2 = 10 ** 9 + 7;
 
 var findLength = function (nums1, nums2) {
   let shortArray;
@@ -63,7 +153,7 @@ var findLength = function (nums1, nums2) {
       if (hashShort < 0) {
         hashShort += MOD;
       }
-      hashShort *= 10;
+      hashShort *= 101;
       hashShort = hashShort % MOD;
       hashShort += newNum;
       hashShort = hashShort % MOD;
@@ -108,7 +198,7 @@ var findLength = function (nums1, nums2) {
       if (hashLong < 0) {
         hashLong += MOD;
       }
-      hashLong *= 10;
+      hashLong *= 101;
       hashLong = hashLong % MOD;
       const newNum = longArray[rLong];
       hashLong += newNum;
