fc37ae011f274872cc112189b5ddbb169d942c85
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-09-01 18:35:42 -0400

    update staging for removed and moved files

--- misc | js 30 days/2621: Sleep.js
+++
@@ -1,9 +0,0 @@
-// https://leetcode.com/problems/sleep/description/
-// Difficulty: Easy
-
-// Solution
-// It's just promisifying a native async function
-
-async function sleep(ms) {
-  return new Promise((res) => setTimeout(res, ms));
-}
