3ffc2c9ca3922c7baf44acfe15105ed3f3481465
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-09-07 21:39:47 -0400

    add reverse linked list sublist template

---
+++ zTemplates Python/linked_lists.py
@@ -0,0 +1,28 @@
+# ______________________________________________________________________
+# REVERSE PORTION OF LINKED LIST TEMPLATE
+# Given a linked list, and a portion l, r, returns the linked list with [l:r] reversed.
+def reverse_sublist(head, l, right):
+        # find the node before left
+        prev = None
+        pointer = head
+        for i in range(l):
+            prev = pointer
+            pointer = pointer.next
+
+        def reverse_list_and_get_next(linked_list, reverse_size):
+            new_tail = linked_list
+            pointer = linked_list
+            prev = None
+            for i in range(reverse_size):
+                next_node = pointer.next
+                pointer.next = prev
+                prev = pointer
+                pointer = next_node
+            new_tail.next = pointer
+            return prev
+
+        rev_list_head = reverse_list_and_get_next(pointer, r - l + 1)
+        if prev:
+            prev.next = rev_list_head
+            return head
+        return rev_list_head
---
+++ zTemplates Python/range_sum_query.py
@@ -0,0 +1,52 @@
+# ______________________________________________________________________
+# IMMUTABLE RANGE SUM QUERY TEMPLATE
+# Variables:
+# ITERABLE - replace with the iterable we need to query
+
+running_sum = 0
+prefix_sums = [] # store sums of prior elements
+for num in ITERABLE:
+    prefix_sums.append(running_sum)
+    running_sum += num
+prefix_sums.append(running_sum)
+
+def sum_query(l, r):
+    return prefix_sums[r + 1] - prefix_sums[l]
+
+
+# ______________________________________________________________________
+# IMMUTABLE RANGE SUM 2D QUERY TEMPLATE
+# Gets the sum for a rectange range query in O(1), after O(n*m) preprocessing
+# Variables:
+# MATRIX - replace with the 2d matrix we need to query
+
+HEIGHT = len(MATRIX)
+WIDTH = len(MATRIX[0])
+# each cell should store the sum for the square from 0,0 to that cell
+prefix_sums = [[0 for _ in range(WIDTH)] for _ in range(HEIGHT)]
+print(prefix_sums)
+for r in range(HEIGHT):
+    for c in range(WIDTH):
+        # the sum is the left prefix plus the top prefix plus the number, minus the up left prefix
+        sum_for_cell = 0
+        if r > 0:
+            sum_for_cell += prefix_sums[r - 1][c]
+        if c > 0:
+            sum_for_cell += prefix_sums[r][c - 1]
+        sum_for_cell += MATRIX[r][c]
+        if r > 0 and c > 0:
+            sum_for_cell -= prefix_sums[r-1][c-1]
+        prefix_sums[r][c] = sum_for_cell
+
+# (row1, col1) form the top left point of the rectangle, (row2, col2) bottom right
+def sumRegion(row1, col1, row2, col2) -> int:
+    # the sum for a region is the bottom right prefix, plus a top left corner prefix, minus a left and a top prefix
+    sum_for_region = 0
+    sum_for_region += prefix_sums[row2][col2]
+    if row1 > 0 and col1 > 0:
+        sum_for_region += prefix_sums[row1 - 1][col1 - 1]
+    if col1 > 0:
+        sum_for_region -= prefix_sums[row2][col1 - 1]
+    if row1 > 0:
+        sum_for_region -= prefix_sums[row1 - 1][col2]
+    return sum_for_region
