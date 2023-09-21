42d22632596a924f9b1ac06ca4b6a887d21f638e
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-09-21 16:49:25 -0400

    367: Valid Perfect Square

---
+++ Math and Geometry/367: Valid Perfect Square.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/valid-perfect-square/description/
+# Difficulty: Easy
+# Tags: Math
+
+# Problem
+
+# Given a positive integer num, return true if num is a perfect square or false otherwise.
+
+# A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+
+# You must not use any built-in library function, such as sqrt.
+
+# Solution, O(log n) or O(1) since integer bound
