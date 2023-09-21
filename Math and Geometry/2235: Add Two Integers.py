ca453faca35e4cab38b15eae9478bc005a828f05
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-09-21 16:46:15 -0400

    2234: Add Two Integers

---
+++ Math and Geometry/2235: Add Two Integers.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/add-two-integers/
+# Difficulty: Easy
+# tags: math
+
+# Problem
+# Given two integers num1 and num2, return the sum of the two integers.
+
+# Solution, O(1) space and time, the easiest question on leetcide!
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
