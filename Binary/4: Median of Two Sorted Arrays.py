261909dfe1cc415181cc41408ecb0af9f7a0e9e3
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-09-21 11:38:51 -0400

    4: Median of Two Sorted Arrays

---
+++ Binary/4: Median of Two Sorted Arrays.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/median-of-two-sorted-arrays/
+# Difficulty: Hard
+# Tags: binary search
+
+# Problem
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # if odd amount of elements, left region should have (n-1)/2 + 1 elements, otherwise n/2
+        if (len(nums1) + len(nums2)) % 2 == 0:
+            leftRegionAmount = (len(nums1) + len(nums2)) / 2
+            medianType = 'even'
+        else:
+            leftRegionAmount = (len(nums1) + len(nums2) - 1)/2 + 1
+            medianType = 'odd'
+        leftRegionAmount = int(leftRegionAmount)
+
+        l = max(0, leftRegionAmount - len(nums2)) # in the worst case, if we include every element from nums2 in the left region, we still might need to include some from nums1
+        r = min(leftRegionAmount, len(nums1))
+
+        # do a binary search on nums 1 to determine some left region, the remaining elements will be pulled from nums2, m is the value such that we include all values to the left of m
+        while l <= r:
+            m = int((r + l) // 2)
+            # amount of elements for the left region
+            amountOfElementsInNums1 = m
+            amountOfElementsInNums2 = leftRegionAmount - m
+            endingRegion1 = nums1[amountOfElementsInNums1 - 1] if amountOfElementsInNums1 > 0 else float('-inf')
+            endingRegion2 = nums2[amountOfElementsInNums2 - 1] if amountOfElementsInNums2 > 0 else float('-inf')
+            nextRegion1 = nums1[amountOfElementsInNums1] if amountOfElementsInNums1 < len(nums1) else float('inf')
+            nextRegion2 = nums2[amountOfElementsInNums2] if amountOfElementsInNums2 < len(nums2) else float('inf')
+
+            if endingRegion1 <= nextRegion2 and endingRegion2 <= nextRegion1:
+                if medianType ==  'even':
+                    biggest = max(endingRegion1, endingRegion2)
+                    smallest = min(nextRegion1, nextRegion2)
+                    return (biggest + smallest) / 2
+                return max(endingRegion1, endingRegion2)
+
+            # if we are too big in nums1, we need to shift to the left
+            if endingRegion1 > nextRegion2:
+                r = m - 1
+            else:
+                l = m + 1
+        return r
+
