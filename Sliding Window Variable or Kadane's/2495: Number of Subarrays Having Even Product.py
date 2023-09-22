92a8fadb3e5c9e4dfffcc2260b5758dc32de316d
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-09-22 17:31:49 -0400

    fix missing solution for last problem

--- Sliding Window Variable or Kadane's/2495: Number of Subarrays Having Even Product.py
+++ Sliding Window Variable or Kadane's/2495: Number of Subarrays Having Even Product.py
@@ -9,3 +9,20 @@
 # Even subarrays total-odds. Odds are ones with no even. Use kadane's to find that.
 
 
+class Solution:
+  def evenProduct(self, nums: List[int]) -> int:
+      n = len(nums)
+      totalSubarrays = n * (n + 1) / 2
+      # find amount of odd subarrays
+
+      oddSubarrays = 0
+      streak = 0 # maintain a kadane's streak for how many odd numbers we have seen in a row
+      for i in range(len(nums)):
+          if nums[i] % 2 == 1:
+              streak += 1
+          else:
+              streak = 0
+          oddSubarrays += streak
+      return int(totalSubarrays - oddSubarrays)
+
+
