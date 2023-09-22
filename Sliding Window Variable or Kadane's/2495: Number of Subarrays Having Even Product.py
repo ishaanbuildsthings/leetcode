90776feb867385db2820b86a6cccaca157f57dcf
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-09-22 17:28:11 -0400

    2495: Number of Subarrays Having Even Product

---
+++ Sliding Window Variable or Kadane's/2495: Number of Subarrays Having Even Product.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/number-of-subarrays-having-even-product/description/
+# difficulty: Medium
+# tags: kadane's
+
+# Problem
+# Given a 0-indexed integer array nums, return the number of subarrays of nums having an even product.
+
+# Solution, O(n) time O(1) space
+# Even subarrays total-odds. Odds are ones with no even. Use kadane's to find that.
+
+
