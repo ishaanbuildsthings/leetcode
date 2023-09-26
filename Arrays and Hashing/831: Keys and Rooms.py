f3eb5fbf63a9907059e9fd371ce04429d99134be
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-09-26 02:50:04 -0400

    841: Keys and Rooms

---
+++ Arrays and Hashing/831: Keys and Rooms.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/keys-and-rooms/description/
+# difficulty: medium
+
+# problem
+# There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room 0. Your goal is to visit all the rooms. However, you cannot enter a locked room without having its key.
+
+# When you visit a room, you may find a set of distinct keys in it. Each key has a number on it, denoting which room it unlocks, and you can take all of them with you to unlock the other rooms.
+
+# Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, return true if you can visit all the rooms, or false otherwise.
+
+# Solution, O(n) time and space
+# Maintain keys we haven't used, keep visitng and adding keys
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        keys = [0]
+        seenRooms = set()
+
+        while len(keys):
+            key = keys.pop()
+            seenRooms.add(key)
+            for newKey in rooms[key]:
+                if newKey in seenRooms:
+                    continue
+                keys.append(newKey)
+
+        return len(seenRooms) == len(rooms)
