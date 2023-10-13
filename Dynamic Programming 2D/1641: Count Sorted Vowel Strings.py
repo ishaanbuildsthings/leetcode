f52c0c22e6bf3f8a318fdf7b8a1b6e196e6f856f
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-10-13 12:48:06 -0400

    1641: Count Sorted Vowel Strings

---
+++ Dynamic Programming 2D/1641: Count Sorted Vowel Strings.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/count-sorted-vowel-strings/description/
+# difficulty: medium
+# tags: dynamic programming 2d, digit dp
+
+# Problem
+# Given an integer n, return the number of strings of length n that consist only of vowels (a, e, i, o, u) and are lexicographically sorted.
+
+# A string s is lexicographically sorted if for all valid i, s[i] is the same as or comes before s[i+1] in the alphabet.
+
+# Solution, O(5 * 5 * n) time, O(5 * n) space
+# Standard digit DP style solution
+
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        @cache
+        def dp(prev, i):
+            # base case
+            if i == n:
+                return 1
+
+            resForThis = 0
+
+            for vowel in 'AEIOU':
+                if vowel < prev:
+                    continue
+                resForThis += dp(vowel, i + 1)
+
+            return resForThis
+        return dp('A', 0)
