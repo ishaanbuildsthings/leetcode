af60a2c901a10885438e32f9d8637c70e41daab5
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-10-18 03:02:06 -0400

    1295: Find Numbers with Even Number of Digits

---
+++ Math and Geometry/1295: Find Numbers with Even Number of Digits.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+# Difficulty: Easy
+# tags: math
+
+# Problem
+# Given an array nums of integers, return how many of them contain an even number of digits.
+
+# Solution, O(n log number) time, O(1) space
+# For each number, do a log operation on it to get the number of digits
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            digits = 0
+            while num:
+                num //= 10
+                digits += 1
+            if digits % 2 == 0:
+                res += 1
+        return res
