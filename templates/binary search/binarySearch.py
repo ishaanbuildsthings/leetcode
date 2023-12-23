f855842dcf377ab7df38b73db9fa14d0edc31f27
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-12-23 09:33:30 -0800

    rearrange folders

---
+++ templates/binary search/binarySearch.py
@@ -0,0 +1,41 @@
+import bisect
+
+# -1 if no number is < threshold
+def firstIndexLT(arr, threshold):
+    index = bisect.bisect_left(arr, threshold)
+    return index - 1 if index > 0 and arr[index - 1] < threshold else -1
+
+# -1 if no number is < threshold
+def lastIndexLT(arr, threshold):
+    index = bisect.bisect_left(arr, threshold)
+    return index - 1 if index > 0 else -1
+
+# -1 if no number is <= threshold
+def firstIndexLTE(arr, threshold):
+    index = bisect.bisect_left(arr, threshold)
+    return index - 1 if index > 0 and arr[index - 1] == threshold else -1
+
+# -1 if no number <= threshold
+def lastIndexLTE(arr, threshold):
+    index = bisect.bisect_right(arr, threshold)
+    return index - 1 if index > 0 else -1
+
+# len(arr) if no number is > threshold
+def firstIndexGT(arr, threshold):
+    index = bisect.bisect_right(arr, threshold - 1)
+    return index if index < len(arr) and arr[index] > threshold else len(arr)
+
+# len(arr) if no number is > threshold
+def lastIndexGT(arr, threshold):
+    index = bisect.bisect_right(arr, threshold)
+    return index if index < len(arr) else len(arr)
+
+# len(arr) if no number is >= threshold
+def firstIndexGTE(arr, threshold):
+    index = bisect.bisect_left(arr, threshold)
+    return index if index < len(arr) else len(arr)
+
+# len(arr) if no number is >= threshold
+def lastIndexGTE(arr, threshold):
+    index = bisect.bisect_right(arr, threshold - 1)
+    return index if index < len(arr) else len(arr)
--- testSuite.py
+++ testSuite.py
@@ -3,36 +3,37 @@ import math
 import collections
 
 outputs = {
-  ((2,3,3,2,2), 2, 2): 11,
-  # (4, 5, 6): 15,
+#   ((2,3,3,2,2), 2, 2): 11,
+#   # (4, 5, 6): 15,
 }
 
 class Solution:
-    def countDistinct(self, nums: List[int], k: int, p: int) -> int:
-        pf = [] # number of elements divisible by p
-        count = 0
-        for num in nums:
-            count += (num / p) == math.floor(num / p)
-            pf.append(count)
-        print(pf)
+   pass
+    # def countDistinct(self, nums: List[int], k: int, p: int) -> int:
+    #     pf = [] # number of elements divisible by p
+    #     count = 0
+    #     for num in nums:
+    #         count += (num / p) == math.floor(num / p)
+    #         pf.append(count)
+    #     print(pf)
 
-        def queryDivis(l, r):
-            if l == 0:
-                return pf[r]
-            return pf[r] - pf[l - 1]
+    #     def queryDivis(l, r):
+    #         if l == 0:
+    #             return pf[r]
+    #         return pf[r] - pf[l - 1]
 
-        seen = set() # stores hashes
+    #     seen = set() # stores hashes
 
-        res = 0
-        for l in range(len(nums)):
-            hash = 0
-            for r in range(l, len(nums)):
-                hash *= 200
-                hash += nums[r]
-                countDivis = queryDivis(l, r)
-                res += countDivis <= k and not hash in seen
-                seen.add(hash)
-        return res
+    #     res = 0
+    #     for l in range(len(nums)):
+    #         hash = 0
+    #         for r in range(l, len(nums)):
+    #             hash *= 200
+    #             hash += nums[r]
+    #             countDivis = queryDivis(l, r)
+    #             res += countDivis <= k and not hash in seen
+    #             seen.add(hash)
+    #     return res
 
 
 sol = Solution()
