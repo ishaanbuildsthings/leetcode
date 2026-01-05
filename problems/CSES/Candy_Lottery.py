88877d87a9a4bccf0e30688b7f749487b65e68f8
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-01-05 18:55:10 -0500

    amend

--- problems/CSES/Candy_Lottery.py
+++ problems/CSES/Candy_Lottery.py
@@ -1,3 +1,8 @@
+# Solution 1
+# Compute the exact change max=X for all X and take their weighted sums
+# First, compute chance of a max being 1
+# From this, we compute chance of maximum being at most 2
+# We can compute the chance max is exactly 2 by subtracting. Repeat this
 from decimal import Decimal, getcontext, ROUND_HALF_EVEN
 
 n, k = map(int, input().split())
@@ -17,3 +22,28 @@ for mx in range(1, k + 1):
     res += weightedContribution
 
 print(res.quantize(Decimal("0.000001"), rounding=ROUND_HALF_EVEN))
+
+
+
+# # Solution 2
+# # Define indicators, I1 = chance max is >= 1, I2 = chance max is >= 2, etc
+# # Now when max is X, we score I1 + I2 + ... + IX
+# # So we need to compute the expected sum of the indicators
+# # Each indicator has a chance of occuring and we can use linearity of expectation to get their probabilities
+# # It just uses inverse probability to compute these
+# from decimal import Decimal, getcontext, ROUND_HALF_EVEN
+
+# n, k = map(int, input().split())
+
+# getcontext().prec = 80
+
+# res = Decimal(0)
+
+# # compute the chance the maximum is >= mx (indicator approach)
+# for mx in range(1, k + 1):
+#     singleChance = Decimal(mx - 1) / Decimal(k)
+#     allChance = singleChance ** Decimal(n)
+#     oddsGte = Decimal(1) - allChance
+#     res += oddsGte
+
+# print(res.quantize(Decimal("0.000001"), rounding=ROUND_HALF_EVEN))
