c4075f2ac6d3a9bd5e56db27bdf90a4a762baaad
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-01-15 23:39:07 -0800

    generate tag

---
+++ problems/Leetcode/3582. Generate Tag for Video Caption.py
@@ -0,0 +1,31 @@
+class Solution:
+    def generateTag(self, caption: str) -> str:        
+        arr = []
+        word = []
+        for c in caption:
+            if c == ' ':
+                if word:
+                    arr.append(''.join(word))
+                    word = []
+            else:
+                word.append(c)
+        if word:
+            arr.append(''.join(word))
+
+        if not arr:
+            return '#'
+
+        for i in range(len(arr)):
+            w = arr[i]
+            w = w.lower()
+            if i != 0:
+                w = w[:1].upper() + w[1:]
+            else:
+                w = w
+            arr[i] = w
+
+        final = '#' + ''.join(arr)
+        final = final[:100]
+        return final
+            
+        
