ec83a0a2388025d8cd29d3a5d247f04a3859c083
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-01-16 12:30:18 -0800

    erase and extend

--- problems/Codeforces/E_1_Erase_and_Extend_Easy_Version.py
+++ problems/Codeforces/E_1_Erase_and_Extend_Easy_Version.py
@@ -1,11 +1,3 @@
-
-# dbca|dabc
-
-
-# dbca dbca
-
-# once we choose to duplicate, it is done and we repeat and then trim
-
 n, k = map(int, input().split())
 s = input()
 
@@ -14,7 +6,6 @@ res = 'z' * k
 for takePf in range(1, n + 1):
     pf = s[:takePf]
     fullFits = k // len(pf)
-    # print(f'full fits: {fullFits} for takepf: {takePf}')
     start = pf * fullFits
     remainLength = k - len(start)
     start += pf[:remainLength]
