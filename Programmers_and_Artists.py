38f284d9c0db80d3528a3bc682d04d89daa2d8f8
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-01-19 02:38:56 -0500

    repetitions

--- Programmers_and_Artists.py
+++
@@ -1,6 +0,0 @@
-numProgrammers, numArtists, numApplications = map(int, input().split())
-skills = [] # holds (programSkill, artSkill)
-for _ in range(n):
-    programSkill, artSkill = map(int, input().split())
-    skills.append([programSkill, artSkill])
-skills.sort(reverse=True) # descending order of program skill
