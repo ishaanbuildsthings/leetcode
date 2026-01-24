67a67c4461406428f29a408f91948e23ef5c7f5f
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-01-23 19:57:32 -0500

    no fun

--- problems/Codeforces/B_Deck_of_Cards.py
+++ problems/Codeforces/B_Deck_of_Cards.py
@@ -1,19 +1,13 @@
 t = int(input())
 for _ in range(t):
-    # print('====')
     n, k = map(int, input().split())
     s = input()
-    # print(f'{s=}')
-    # print(f'{n=}')
-
     # a card X is guaranteed removed if X tops were taken, N - X + 1 bottoms were taken
     # a card is guaranteed in the deck if X tops + eithers cannot reach it, or the bottoms + eithers could not reach it
 
     tops = sum(x == '0' for x in s)
     bottoms = sum(x == '1' for x in s)
     either = sum(x == '2' for x in s)
-
-    # print(f'{tops=} {bottoms=} {either=}')
 
     res = []
 
@@ -22,7 +16,6 @@ for _ in range(t):
         if tops >= card:
             res.append('-')
             continue
-        # print(f'{card=}')
         if card >= n - bottoms + 1:
             res.append('-')
             continue
