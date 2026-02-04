b4cb472cab175a67763dca20fedc41975ef0b3d4
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-02-03 20:53:58 -0500

    primes

---
+++ problems/Leetcode/3591. Check if Any Element Has Prime Frequency.py
@@ -0,0 +1,27 @@
+# template by: https://github.com/agrawalishaan/leetcode
+# O(n) time / space
+class PrimeSieve:
+    def __init__(self, n):
+        self.sieve = [True for _ in range(n)]
+        self.sieve[0] = False
+        self.sieve[1] = False
+        for i in range(2, n):
+            if self.sieve[i]: # linear optimization
+                for j in range(i * i, n, i):
+                    self.sieve[j] = False
+        self.primeList = [i for i in range(n) if self.sieve[i]]
+
+    def isPrime(self, n):
+        return self.sieve[n]
+
+    def getPrimeList(self):
+        return self.primeList
+sieve = PrimeSieve(1000)
+class Solution:
+    def checkPrimeFrequency(self, nums: List[int]) -> bool:
+        c = Counter(nums)
+        for key in c:
+            if sieve.isPrime(c[key]):
+                return True
+        return False
+        
