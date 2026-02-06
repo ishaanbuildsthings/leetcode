b6d7438d8766aaae33ab8a207b7525e06b073aff
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-02-06 17:32:52 -0500

    sql tax

---
+++ problems/Leetcode/sql/1327. List the Products Ordered in a Period.sql
@@ -0,0 +1,2 @@
+-- Write your PostgreSQL query statement below
+SELECT product_name, SUM(unit) AS unit FROM Products JOIN Orders ON Products.product_id = Orders.product_id WHERE order_date >= '2020-02-01' AND order_date <= '2020-02-29' GROUP BY product_name HAVING SUM(Orders.unit) >= 100;
