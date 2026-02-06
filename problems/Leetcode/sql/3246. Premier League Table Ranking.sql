f17446f70f8889dc010be248eb8c0064419907de
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-02-06 02:27:36 -0500

    premiers

---
+++ problems/Leetcode/sql/3246. Premier League Table Ranking.sql
@@ -0,0 +1,2 @@
+-- Write your PostgreSQL query statement below
+SELECT team_id, team_name, (3 * wins + draws) AS points, RANK() OVER (ORDER BY (wins * 3 + draws) DESC) AS position FROM TeamStats ORDER BY points DESC, team_name ASC;
