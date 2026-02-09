74b08f32bbe0b3c8c5eb7a47f3d0b0ae6a7163d1
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-02-09 02:16:21 -0500

    x

--- problems/CSES/Polygon_Area.py
+++ problems/CSES/Polygon_Area.py
@@ -3,7 +3,7 @@ points = []
 for _ in range(n):
     x, y = map(int, input().split())
     points.append((x, y))
-res = 0
+
 for i in range(n):
     x, y = points[i]
     x2, y2 = points[(i + 1) % n]
