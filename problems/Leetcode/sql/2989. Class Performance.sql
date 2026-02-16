f70d4b38679225e22b60e2ae3359611c8ba86f09
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-02-16 17:32:05 -0500

    fix

--- problems/Leetcode/sql/2989. Class Performance.sql
+++ problems/Leetcode/sql/2989. Class Performance.sql
@@ -1,4 +1,4 @@
 -- Write your PostgreSQL query statement below
 SELECT (MAX(tot) - MIN(tot)) AS difference_in_score FROM (
-    SELECT SUM(assignment1 + assignment2 + assignment3) AS tot FROM Scores GROUP BY student_id
+    SELECT (assignment1 + assignment2 + assignment3) AS tot FROM Scores
 );
