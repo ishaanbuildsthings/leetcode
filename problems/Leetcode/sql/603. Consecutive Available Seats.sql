e7fb209d0b07ed354eb12fb0295cd16932edc66b
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-02-25 16:55:34 -0500

    seats

---
+++ problems/Leetcode/sql/603. Consecutive Available Seats.sql
@@ -0,0 +1,5 @@
+SELECT seat_id FROM Cinema WHERE
+free = 1 AND
+((seat_id + 1) in (SELECT seat_id FROM Cinema WHERE free = 1) OR
+(seat_id - 1) in (SELECT seat_id FROM Cinema WHERE free = 1))
+ORDER BY seat_id;
