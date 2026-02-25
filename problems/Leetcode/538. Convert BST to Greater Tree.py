9a59c00f2ce19a7c5eb3a74ecfe3f2446855b302
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-02-25 18:24:13 -0500

    538

--- problems/Leetcode/538. Convert BST to Greater Tree.py
+++ problems/Leetcode/538. Convert BST to Greater Tree.py
@@ -6,20 +6,30 @@
 #         self.right = right
 class Solution:
     def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return root
-        
-        # a node needs to add anything in the right subtree
-        # and add anything from above it
-        def dfs(node, sumFromAbove):
-            if not node:
-                return 0
-            rightSum = dfs(node.right, sumFromAbove) # this line is confusing
-            addingToLeft = sumFromAbove + node.val + rightSum
-            orig = node.val
-            node.val += sumFromAbove + rightSum
-            leftSum = dfs(node.left, addingToLeft)
-            return leftSum + rightSum + orig
-        dfs(root, 0)
+        # # a node needs to add anything in the right subtree
+        # # and add anything from above it
+        # def dfs(node, sumFromAbove):
+        #     if not node:
+        #         return 0
+        #     rightSum = dfs(node.right, sumFromAbove) # this line is confusing
+        #     addingToLeft = sumFromAbove + node.val + rightSum
+        #     orig = node.val
+        #     node.val += sumFromAbove + rightSum
+        #     leftSum = dfs(node.left, addingToLeft)
+        #     return leftSum + rightSum + orig
+        # dfs(root, 0)
 
+        # return root
+
+        runningSum = 0
+        def revInorder(node):
+            nonlocal runningSum
+            if not node:
+                return
+            revInorder(node.right)
+            newVal = node.val + runningSum
+            runningSum += node.val
+            node.val = newVal
+            revInorder(node.left)
+        revInorder(root)
         return root
