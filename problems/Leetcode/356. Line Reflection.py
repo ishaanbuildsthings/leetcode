0b51fd565c4c3037919613ee41f76270963fb506
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-03-02 17:18:58 -0500

    line

---
+++ problems/Leetcode/356. Line Reflection.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isReflected(self, points: List[List[int]]) -> bool:
+        leftmostX = min(p[0] for p in points)
+        rightmostX = max(p[0] for p in points)
+        mid = (rightmostX+leftmostX) / 2
+        c = Counter()
+        for x, y in points:
+            c[tuple([x, y])] += 1
+        
+        skipGroup = Counter()
+        for x, y in points:
+
+            dist = abs(mid - x)
+            if x <= mid:
+                refX = mid + dist
+            else:
+                refX = mid - dist
+            
+            t2 = tuple([refX, y])
+            if c[t2] == 0:
+                return False
+        
+        return True
