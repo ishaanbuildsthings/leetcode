0d2b32397e4e5f9bf6cfff828c3b6304c1913b3d
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-03-04 20:37:51 -0500

    leaf

---
+++ problems/Leetcode/1130. Minimum Cost Tree From Leaf Values.py
@@ -0,0 +1,44 @@
+# MAX sparse table
+# O(n log n) build
+# O(1) range MAX
+
+class SparseMax:
+    def __init__(self, arr):
+        self.n = len(arr)
+        self.LOG = self.n.bit_length()
+        self.sparse = [[0] * self.n for _ in range(self.LOG)]
+        for i in range(self.n):
+            self.sparse[0][i] = arr[i]
+        for power in range(1, self.LOG):
+            halfWidth = 1 << (power - 1)
+            for left in range(self.n):
+                val = self.sparse[power - 1][left]
+                rightEdge = left + halfWidth
+                if rightEdge < self.n:
+                    val = max(val, self.sparse[power - 1][rightEdge])
+                self.sparse[power][left] = val
+
+    def query(self, l, r):
+        width = r - l + 1
+        maxPow = width.bit_length() - 1
+        powWidth = 1 << maxPow
+        return max(
+            self.sparse[maxPow][l],
+            self.sparse[maxPow][l + width - powWidth]
+        )
+
+
+class Solution:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+        s = SparseMax(arr)
+        @cache
+        def dp(l, r):
+            if l == r:
+                return 0
+            res = inf
+            for left in range(l, r):
+                gain = dp(l, left) + dp(left + 1, r) + s.query(l, left) * s.query(left + 1, r) # could compute all the maxes above this for loop instead lol
+                res = min(res, gain)
+            return res
+        
+        return dp(0, len(arr) - 1)
