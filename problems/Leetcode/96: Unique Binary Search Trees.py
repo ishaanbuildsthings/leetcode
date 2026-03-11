464b4be7780e5c457f66fafbe7a2996815642cca
ishaan agrawal <ishaan.agrawal@berkeley.edu>
2026-03-11 16:34:47 -0400

    more catalan

---
+++ problems/Leetcode/96: Unique Binary Search Trees.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        @cache
+        def dp(l, r):
+            if l == r:
+                return 1
+            if l > r:
+                return 1
+            res = 0
+            for rt in range(l, r + 1):
+                left = dp(l, rt - 1)
+                right = dp(rt + 1, r)
+                res += left * right
+            return res
+        return dp(1, n) 
