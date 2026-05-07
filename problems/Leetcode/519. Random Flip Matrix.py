6c0bfeab205b17db029410422b49c8f0c558bb8d
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-05-06 20:23:11 -0400

    update

--- problems/Leetcode/519. Random Flip Matrix.py
+++ problems/Leetcode/519. Random Flip Matrix.py
@@ -1,3 +1,53 @@
+# APPROACH 0, binary search with blacklist, log^2 n
+
+class Solution:
+
+    def __init__(self, m: int, n: int):
+        self.black = SortedList()
+        self.cells = m * n
+        self.m = m
+        self.n = n
+
+    def flip(self) -> List[int]:
+        iRange = self.cells - len(self.black) - 1 # we go from 0...iRange
+        randI = random.randint(0, iRange)
+        # binary search for smallest index that would be the fifth element
+        l = 0
+        r = self.cells - 1
+        res = None
+        while l <= r:
+            m = (l + r) // 2
+            cntLte = self.black.bisect_right(m)
+            newI = m - cntLte
+            if newI < randI:
+                l = m + 1
+            elif newI == randI:
+                res = m
+                r = m - 1
+            else:
+                r = m - 1
+        
+        trueIdx = res
+        row = trueIdx // self.n
+        col = trueIdx % self.n
+        self.black.add(trueIdx)
+        return row, col
+
+        
+
+    def reset(self) -> None:
+        self.black = SortedList()
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(m, n)
+# param_1 = obj.flip()
+# obj.reset()
+
+
+
+
+# APPROACH 1, O(1) time everything, swapping + virtual array
 class Solution:
 
     def __init__(self, m: int, n: int):
