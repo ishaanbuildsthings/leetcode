ccc21dbdc4e7491967e81b94bc619ae3e2c3d3c1
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-05-20 11:56:19 -0400

    update

---
+++ problems/Leetcode/sql/512. Game Play Analysis II.sql
@@ -0,0 +1,4 @@
+-- Write your PostgreSQL query statement below
+SELECT player_id, device_id
+FROM Activity
+WHERE (player_id, event_date) IN ( SELECT player_id, MIN(event_date) FROM Activity GROUP BY player_id);
