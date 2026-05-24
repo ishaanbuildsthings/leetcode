580ea1dd63dcbd7c844657e15592054acff603b4
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-05-23 20:05:47 -0400

    update

---
+++ problems/Codeforces/D_Distance_in_Tree.py
@@ -0,0 +1,236 @@
+standard_input, packages, output_together = 1, 1, 0
+dfs, hashing, read_from_file = 1, 0, 0
+de = 0
+
+if 1:
+
+    if standard_input:
+        import io, os, sys
+        input = lambda: sys.stdin.readline().strip()
+
+        import math
+        inf = math.inf
+
+        def I():
+            return input()
+
+        def II():
+            return int(input())
+
+        def MII():
+            return map(int, input().split())
+
+        def LI():
+            return input().split()
+
+        def LII():
+            return list(map(int, input().split()))
+
+        def LFI():
+            return list(map(float, input().split()))
+
+        def GMI():
+            return map(lambda x: int(x) - 1, input().split())
+
+        def LGMI():
+            return list(map(lambda x: int(x) - 1, input().split()))
+
+    if packages:
+        from io import BytesIO, IOBase
+
+        import random
+        import os
+
+        import bisect
+        import typing
+        from collections import Counter, defaultdict, deque
+        from copy import deepcopy
+        from functools import cmp_to_key, lru_cache, reduce
+        from heapq import merge, heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+        from itertools import accumulate, combinations, permutations, count, product
+        from operator import add, iand, ior, itemgetter, mul, xor
+        from string import ascii_lowercase, ascii_uppercase, ascii_letters
+        from typing import *
+        BUFSIZE = 4096
+
+    if output_together:
+        class FastIO(IOBase):
+            newlines = 0
+
+            def __init__(self, file):
+                self._fd = file.fileno()
+                self.buffer = BytesIO()
+                self.writable = "x" in file.mode or "r" not in file.mode
+                self.write = self.buffer.write if self.writable else None
+
+            def read(self):
+                while True:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    if not b:
+                        break
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines = 0
+                return self.buffer.read()
+
+            def readline(self):
+                while self.newlines == 0:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    self.newlines = b.count(b"\n") + (not b)
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines -= 1
+                return self.buffer.readline()
+
+            def flush(self):
+                if self.writable:
+                    os.write(self._fd, self.buffer.getvalue())
+                    self.buffer.truncate(0), self.buffer.seek(0)
+
+        class IOWrapper(IOBase):
+            def __init__(self, file):
+                self.buffer = FastIO(file)
+                self.flush = self.buffer.flush
+                self.writable = self.buffer.writable
+                self.write = lambda s: self.buffer.write(s.encode("ascii"))
+                self.read = lambda: self.buffer.read().decode("ascii")
+                self.readline = lambda: self.buffer.readline().decode("ascii")
+
+        sys.stdout = IOWrapper(sys.stdout)
+
+    if dfs:
+        from types import GeneratorType
+
+        def bootstrap(f, stk=[]):
+            def wrappedfunc(*args, **kwargs):
+                if stk:
+                    return f(*args, **kwargs)
+                else:
+                    to = f(*args, **kwargs)
+                    while True:
+                        if type(to) is GeneratorType:
+                            stk.append(to)
+                            to = next(to)
+                        else:
+                            stk.pop()
+                            if not stk:
+                                break
+                            to = stk[-1].send(to)
+                    return to
+            return wrappedfunc
+
+    if hashing:
+        RANDOM = random.getrandbits(20)
+        class Wrapper(int):
+            def __init__(self, x):
+                int.__init__(x)
+
+            def __hash__(self):
+                return super(Wrapper, self).__hash__() ^ RANDOM
+
+    if read_from_file:
+        file = open("input.txt", "r").readline().strip()[1:-1]
+        fin = open(file, 'r')
+        input = lambda: fin.readline().strip()
+        output_file = open("output.txt", "w")
+        def fprint(*args, **kwargs):
+            print(*args, **kwargs, file=output_file)
+
+    if de:
+        def debug(*args, **kwargs):
+            # print('\033[92m', end='')
+            print(*args, **kwargs)
+            # print('\033[0m', end='')
+    else:
+        def debug(*args, **kwargs):
+            pass
+
+
+    fmax = lambda x, y: x if x > y else y
+    fmin = lambda x, y: x if x < y else y
+
+    class lst_lst:
+        def __init__(self, n) -> None:
+            self.n = n
+            self.pre = []
+            self.cur = []
+            self.notest = [-1] * (n + 1)
+
+        def append(self, i, j):
+            self.pre.append(self.notest[i])
+            self.notest[i] = len(self.cur)
+            self.cur.append(j)
+
+        def iterate(self, i):
+            tmp = self.notest[i]
+            while tmp != -1:
+                yield self.cur[tmp]
+                tmp = self.pre[tmp]
+
+n, k = LII()
+
+# debug(f'{n=}, {k=}')
+edges = []
+for _ in range(n - 1):
+    a, b = LII()
+    edges.append((a, b))
+# debug(f'{edges=}')
+
+# TEMPLATE BY ISHAAN AGRAWAL, github: ishaanbuildsthings
+# takes nodes from 0 to n-1 and constructs a children map rooted at 0
+from collections import defaultdict
+
+def edgeListToTree(edgeList):
+    edgeMap = defaultdict(list)
+    for a, b in edgeList:
+        edgeMap[a].append(b)
+        edgeMap[b].append(a)
+
+    children = defaultdict(list) # maps a node to its children
+
+    stack = [(1, -1)]  # (node, parent)
+    while stack:
+        node, parent = stack.pop()
+        for adj in edgeMap[node]:
+            if adj == parent:
+                continue
+            children[node].append(adj)
+            stack.append((adj, node))
+
+    return children
+
+children = edgeListToTree(edges)
+# debug(f'{children=}')
+res = 0
+
+@bootstrap
+def dfs(node):
+    global res
+    resHere = 0
+    dp = [0] * (k + 1) # dp[x] is the number of nodes distance x away from this, below it
+    dp[0] = 1 # root
+
+    # we will also aggregate the answer below us
+    for child in children[node]:
+        ndp = dp[:]
+        childDp = yield dfs(child)
+        for childDist in range(k):
+            newDist = childDist + 1
+            reqDist = k - newDist
+            if reqDist < 0 or reqDist >= len(dp):
+                continue
+            resHere += dp[reqDist] * childDp[childDist]
+            ndp[newDist] += childDp[childDist]
+        dp = ndp
+    res += resHere
+    # debug(f'{resHere=} for node: {node=}')
+
+    yield dp
+
+dfs(1)
+# debug(f'{res=}')
+
+print(res)
+
+
+ 
