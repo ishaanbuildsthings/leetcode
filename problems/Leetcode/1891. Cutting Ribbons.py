632e3cd122d23f45afe0f85831c3e30c86050469
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-05-24 13:37:30 -0400

    update

---
+++ problems/Leetcode/1891. Cutting Ribbons.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxLength(self, ribbons: List[int], k: int) -> int:
+
+        def isLengthDoable(length):
+            count = 0
+            for ribbon in ribbons:
+                count += ribbon // length
+                if count >= k:
+                    return True
+            return False
+
+        l = 1
+        r = max(ribbons)
+        while l <= r:
+            m = (r + l) // 2
+            if isLengthDoable(m):
+                l = m + 1
+            else:
+                r = m - 1
+        return l - 1
