109e9d8b267e8edcbd8dcce50f03409cd026ac9a
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-06-02 23:10:41 -0400

    update

--- problems/Leetcode/1271. Hexspeak.py
+++ problems/Leetcode/1271. Hexspeak.py
@@ -1,6 +1,17 @@
 class Solution:
     def toHexspeak(self, num: str) -> str:
-        hexed = hex(int(num))[2:].replace('0', 'O').replace('1', 'I').upper()
-        if any(s not in 'ABCDEFIO' for s in hexed):
+        # convert to base 16
+        num = int(num)
+        base16 = []
+        while num:
+            last = num % 16
+            base16.append(last)
+            num -= last
+            num //= 16
+        base16 = base16[::-1]
+        base16 = [
+            str(x) if x <= 9 else 'ABCDEF'[x - 10] for x in base16]
+        base16 = ['I' if x == '1' else 'O' if x == '0' else x for x in base16]
+        if any(x not in 'ABCDEFGIO' for x in base16):
             return 'ERROR'
-        return hexed
+        return ''.join(base16)
