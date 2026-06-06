d434c72027111d37746b70edac98190843be43e0
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-06-05 20:37:21 -0700

    update

--- problems/Leetcode/1682: Longest Palindromic Subsequence II.py
+++ problems/Leetcode/1682: Longest Palindromic Subsequence II.py
@@ -29,14 +29,17 @@ class Solution:
                 opt1 = dp(l + 1, r)
                 opt2 = dp(l, r - 1)
                 if opt1[0] >= opt2[0]:
-                    return [opt1[0], opt2[0], opt1[2]]
-                return [opt2[0], opt1[0], opt2[2]]
+                    # key behavior here, we have to make sure we don't inadvertently allow two options that used the same letter
+                    secondary = opt2[0] if opt2[2] != opt1[2] else max(opt1[1], opt2[1])
+                    return [opt1[0], secondary, opt1[2]]
+                secondary = opt1[0] if opt1[2] != opt2[2] else max(opt2[1], opt1[1])
+                return [opt2[0], secondary, opt2[2]]
             
             inner = dp(l + 1, r - 1)
             if s[l] != inner[2]:
                 return [1 + inner[0], inner[0], s[l]]
             # if the chars do match we cannot use it
-            return [inner[0], 1 + inner[1], inner[2]]
+            # this line might be able to be simplified a bit
+            return [max(1 + inner[1], inner[0]), max(inner[0], inner[1]), s[l]]
         
         ans = dp(0, len(s) - 1)
-        return ans[0] * 2
