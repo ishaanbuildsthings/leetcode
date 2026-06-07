2e0c3743c75285e12857c0dd2f0a476b0a88722a
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-06-07 10:29:31 -0700

    update

--- problems/Leetcode/476. Number Complement.py
+++ problems/Leetcode/476. Number Complement.py
@@ -1,5 +1,5 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        msb = num.bit_length() - 1
+        msb = num.bit_length()
         fmask = (1 << msb) - 1
         return fmask ^ num
