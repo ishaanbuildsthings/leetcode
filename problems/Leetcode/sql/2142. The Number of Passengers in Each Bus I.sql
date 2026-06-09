677cee1df3c7127a03d0cf15ffe8b5121d950965
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-06-09 08:30:40 -0700

    update

--- problems/Leetcode/sql/2142. The Number of Passengers in Each Bus I.sql
+++ problems/Leetcode/sql/2142. The Number of Passengers in Each Bus I.sql
@@ -1,15 +1,22 @@
 -- Write your PostgreSQL query statement below
+WITH bus AS (
 SELECT
 b.bus_id,
-COUNT(p.passenger_id) AS passengers_cnt
-FROM Buses b
-LEFT JOIN Passengers p
-ON p.arrival_time <= b.arrival_time
-AND p.arrival_time > COALESCE(
+b.arrival_time,
+COALESCE(
 (SELECT MAX(b2.arrival_time)
 FROM Buses b2
 WHERE b2.arrival_time < b.arrival_time),
 -1
+) AS prev
+FROM Buses b
 )
-GROUP BY b.bus_id, b.arrival_time
-ORDER BY b.bus_id;
+SELECT
+bus.bus_id,
+COUNT(p.passenger_id) AS passengers_cnt
+FROM bus
+LEFT JOIN Passengers p
+ON p.arrival_time <= bus.arrival_time
+AND p.arrival_time > bus.prev
+GROUP BY bus.bus_id, bus.arrival_time
+ORDER BY bus.bus_id;
