067e76bb57fde5e41e7041d6bf59a968dae1395b
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-07-19 19:47:51 -0700

    update

---
+++ problems/Leetcode/157. Read N Characters Given Read4.py
@@ -0,0 +1,35 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+
+        totalFinished = 0
+        while totalFinished < n:
+            buf4 = ['','','','']
+            cnt = read4(buf4)
+            used = min(n - totalFinished, cnt)
+            for idx in range(totalFinished, totalFinished + used):
+                buf[idx] = buf4[idx - totalFinished]
+            totalFinished += used
+            if cnt < 4: break # safety if entire file is small
+        return totalFinished
+            
+        
