671ff3d7fff21fd6f6751f6b7f871fcd243b50ab
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-07-28 22:41:58 -0700

    update

---
+++ problems/Leetcode/1175. Prime Arrangements.py
@@ -0,0 +1,39 @@
+# template by: https://github.com/agrawalishaan/leetcode
+# O(n) time / space
+class PrimeSieve:
+    def __init__(self, n):
+        self.sieve = [True for _ in range(n)]
+        self.sieve[0] = False
+        self.sieve[1] = False
+        for i in range(2, n):
+            if self.sieve[i]: # linear optimization
+                for j in range(i * i, n, i):
+                    self.sieve[j] = False
+        self.primeList = [i for i in range(n) if self.sieve[i]]
+
+    def isPrime(self, n):
+        return self.sieve[n]
+
+    def getPrimeList(self):
+        return self.primeList
+
+
+
+class Solution:
+    def numPrimeArrangements(self, n: int) -> int:
+        sieve = PrimeSieve(n + 1)
+
+        primes = len(sieve.getPrimeList())
+        unPrime = n - primes
+        print(sieve.getPrimeList())
+
+        res = 1
+        MOD = 10**9 + 7
+        for x in range(1, primes + 1):
+            res *= x
+            res %= MOD
+        for x in range(1, unPrime + 1):
+            res *= x
+            res %= MOD
+        
+        return res
