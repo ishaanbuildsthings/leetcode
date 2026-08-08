df8807444a32b04d9151a7f7563e789943b8d662
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-08-08 11:06:16 -0700

    update

--- problems/Leetcode/1197: Minimum Knight Moves.py
+++ problems/Leetcode/1197: Minimum Knight Moves.py
@@ -1,43 +1,52 @@
-# https://leetcode.com/problems/minimum-knight-moves/
-# difficulty: medium
-# tags: matrix bfs
-
-# problem
-# In an infinite chess board with coordinates from -infinity to +infinity, you have a knight at square [0, 0].
-
-# A knight has 8 possible moves it can make, as illustrated below. Each move is two squares in a cardinal direction, then one square in an orthogonal direction.
-
-# Return the minimum number of steps needed to move the knight to the square [x, y]. It is guaranteed the answer exists.
-
-# Solution, O(x*y) time and space but really there is a tighter boundary. Better seen hash keys can also be done. Pruning can definitely be added / a greedy direction algorithm. But pretty sure an O(1) solution exists also!
-
 class Solution:
-    def minKnightMoves(self, targetX: int, targetY: int) -> int:
-        # edge case
-        if targetX == 0 and targetY == 0:
-            return 0
+    def minKnightMoves(self, x: int, y: int) -> int:
+        x = abs(x)
+        y = abs(y)
+        x, y = max(x, y), min(x, y)
 
-        queue = collections.deque()
-        queue.append([0, 0])
-        level = 0
+        bound1 = ceil(x / 2)
+        bound2 = ceil((x + y) / 3)
+        moves = max(bound1, bound2)
 
-        diffs = [ [1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1] ]
+        p1 = (0) + moves % 2 # color we are on after this many moves, we start on x%2==y%2 and alternate
+        p2 = (x + y) % 2 # color we want to be on
 
-        seen = set() # holds 'x,y'
-        seen.add('0,0')
-        while True:
-            queueLength = len(queue)
-            level += 1
-            for _ in range(queueLength):
-                x, y = queue.popleft() # pretend real queue O(1)
-                for rowDiff, colDiff in diffs:
-                    newRow = x + rowDiff
-                    newCol = y + colDiff
-                    if newRow == targetX and newCol == targetY:
-                        return level
-                    key = f'{newRow},{newCol}'
-                    if key in seen:
-                        continue
-                    seen.add(key)
-                    queue.append([newRow, newCol])
+        if (x, y) == (1, 0):
+            return 3
+        if (x, y) == (2, 2):
+            return 4
+
+        if p1 != p2:
+            moves += 1
+        
+        return moves
+
+
+        # # edge case
+        # if targetX == 0 and targetY == 0:
+        #     return 0
+
+        # queue = collections.deque()
+        # queue.append([0, 0])
+        # level = 0
+
+        # diffs = [ [1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1] ]
+
+        # seen = set() # holds 'x,y'
+        # seen.add('0,0')
+        # while True:
+        #     queueLength = len(queue)
+        #     level += 1
+        #     for _ in range(queueLength):
+        #         x, y = queue.popleft() # pretend real queue O(1)
+        #         for rowDiff, colDiff in diffs:
+        #             newRow = x + rowDiff
+        #             newCol = y + colDiff
+        #             if newRow == targetX and newCol == targetY:
+        #                 return level
+        #             key = f'{newRow},{newCol}'
+        #             if key in seen:
+        #                 continue
+        #             seen.add(key)
+        #             queue.append([newRow, newCol])
 
