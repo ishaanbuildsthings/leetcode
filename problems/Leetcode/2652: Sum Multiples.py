c8c374586a08c555168debde66c4a83a26961dfa
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-08-17 09:32:52 -0700

    update

--- problems/Leetcode/2652: Sum Multiples.py
+++ problems/Leetcode/2652: Sum Multiples.py
@@ -1,18 +1,6 @@
-# https://leetcode.com/problems/sum-multiples/description/
-# difficulty: easy
-# tags: math
-
-# problem
-# Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
-
-# Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
-
-# Solution, O(n) time, O(1) space, for each number try the divisors
-
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
-        res = 0
-        for num in range(1, n + 1):
-            if num % 3 == 0 or num % 5 == 0 or num % 7 == 0:
-                res += num
-        return res
+        def fn(v):
+            facs = n // v
+            return v * (facs * (facs + 1) // 2)
+        return fn(3) + fn(5) + fn(7) - fn(15) - fn(21) - fn(35) + fn(105)
