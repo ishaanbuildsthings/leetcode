661f172049b5e5a26fa40f1f169ab3bf3dd5f7e5
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-08-31 21:06:27 -0700

    update

--- problems/Leetcode/1578: Minimum Time to Make Rope Colorful.py
+++ problems/Leetcode/1578: Minimum Time to Make Rope Colorful.py
@@ -1,39 +1,61 @@
-# https://leetcode.com/problems/minimum-time-to-make-rope-colorful/description/?envType=daily-question&envId=2023-12-27
-# difficulty: medium
-# tags: greedy, dynamic programming 2d
-
-# Problem
-# Alice has n balloons arranged on a rope. You are given a 0-indexed string colors where colors[i] is the color of the ith balloon.
-
-# Alice wants the rope to be colorful. She does not want two consecutive balloons to be of the same color, so she asks Bob for help. Bob can remove some balloons from the rope to make it colorful. You are given a 0-indexed integer array neededTime where neededTime[i] is the time (in seconds) that Bob needs to remove the ith balloon from the rope.
-
-# Return the minimum time Bob needs to make the rope colorful.
-
-# Solution (greedy), for each consecutive group keep only the best one. I was dumb and did a 26n dp instead.
-
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
 
-        memo = [defaultdict(lambda: -1) for _ in range(len(colors))]
-        def dp(i, prevColor):
-            # base
+        # returns two things (minTime1, rightColor), (minTime2, differentRightColor)
+        @cache
+        def dp(i):
             if i == len(colors):
-                return 0
+                return (0, -1), (0, 1)
+            t = neededTime[i]
+            v = colors[i]
 
-            if memo[i][prevColor] != -1:
-                return memo[i][prevColor]
+            # these are thw two states the suffix can give me
+            (time1, c1), (time2, c2) = dp(i + 1)
 
-            resThis = float('inf')
+            # if our color is the same as the best next color, we must either pop ours and use that next color, or tack ours onto the second best, these both give the option ending with our balloon color though
 
-            # we can skip this if we are a different color
-            if colors[i] != prevColor:
-                resThis = dp(i + 1, colors[i])
+            # for our 2nd best, we would necessarily pop ours and pair with 2nd best
+            if v == c1:
+                TIME_A = min(time2, time1 + t)
+                COLOR_A = v
 
-            # we can remove this
-            ifRemoveThis = neededTime[i] + dp(i + 1, prevColor)
+                TIME_B = time2 + t
+                COLOR_B = c2
 
-            resThis = min(resThis, ifRemoveThis)
-            memo[i][prevColor] = resThis
-            return resThis
+                return (TIME_A, COLOR_A), (TIME_B, COLOR_B)
+            
+            # if our color is different from the next best, we could pair our balloon onto that for the for sure new minimum time
+            # for the second minimum time, we could pop our balloon and use the best one, these are going to be the top 2
+            return (time1, v), (t + time1, c1)
+        
+        return dp(0)[0][0]
+            
+            
+        
+            
 
-        return dp(0, None)
+
+
+        # memo = [defaultdict(lambda: -1) for _ in range(len(colors))]
+        # def dp(i, prevColor):
+        #     # base
+        #     if i == len(colors):
+        #         return 0
+
+        #     if memo[i][prevColor] != -1:
+        #         return memo[i][prevColor]
+
+        #     resThis = float('inf')
+            
+        #     # we can skip this if we are a different color
+        #     if colors[i] != prevColor:
+        #         resThis = dp(i + 1, colors[i])
+
+        #     # we can remove this
+        #     ifRemoveThis = neededTime[i] + dp(i + 1, prevColor)
+
+        #     resThis = min(resThis, ifRemoveThis)
+        #     memo[i][prevColor] = resThis
+        #     return resThis
+        
+        # return dp(0, None)
