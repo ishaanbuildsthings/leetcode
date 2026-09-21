ec6ce9f528591836c5e8a5ddd0287746ca7c51a1
Ishaan Agrawal <ishaanbuildsthings@users.noreply.github.com>
2026-09-20 17:22:26 -0700

    update

---
+++ problems/Codeforces/E_1_Three_Blocks_Palindrome_easy_version.py
@@ -0,0 +1,237 @@
+standard_input, packages, output_together = 1, 1, 0
+dfs, hashing, read_from_file = 0, 0, 0
+de = 0
+
+if 1:
+
+    if standard_input:
+        import io, os, sys
+        input = lambda: sys.stdin.readline().strip()
+
+        import math
+        inf = math.inf
+
+        def I():
+            return input()
+
+        def II():
+            return int(input())
+
+        def MII():
+            return map(int, input().split())
+
+        def LI():
+            return input().split()
+
+        def LII():
+            return list(map(int, input().split()))
+
+        def LFI():
+            return list(map(float, input().split()))
+
+        def GMI():
+            return map(lambda x: int(x) - 1, input().split())
+
+        def LGMI():
+            return list(map(lambda x: int(x) - 1, input().split()))
+
+    if packages:
+        from io import BytesIO, IOBase
+
+        import random
+        import os
+
+        import bisect
+        import typing
+        from collections import Counter, defaultdict, deque
+        from copy import deepcopy
+        from functools import cmp_to_key, lru_cache, reduce
+        from heapq import merge, heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+        from itertools import accumulate, combinations, permutations, count, product
+        from operator import add, iand, ior, itemgetter, mul, xor
+        from string import ascii_lowercase, ascii_uppercase, ascii_letters
+        from typing import *
+        BUFSIZE = 4096
+
+    if output_together:
+        class FastIO(IOBase):
+            newlines = 0
+
+            def __init__(self, file):
+                self._fd = file.fileno()
+                self.buffer = BytesIO()
+                self.writable = "x" in file.mode or "r" not in file.mode
+                self.write = self.buffer.write if self.writable else None
+
+            def read(self):
+                while True:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    if not b:
+                        break
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines = 0
+                return self.buffer.read()
+
+            def readline(self):
+                while self.newlines == 0:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    self.newlines = b.count(b"\n") + (not b)
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines -= 1
+                return self.buffer.readline()
+
+            def flush(self):
+                if self.writable:
+                    os.write(self._fd, self.buffer.getvalue())
+                    self.buffer.truncate(0), self.buffer.seek(0)
+
+        class IOWrapper(IOBase):
+            def __init__(self, file):
+                self.buffer = FastIO(file)
+                self.flush = self.buffer.flush
+                self.writable = self.buffer.writable
+                self.write = lambda s: self.buffer.write(s.encode("ascii"))
+                self.read = lambda: self.buffer.read().decode("ascii")
+                self.readline = lambda: self.buffer.readline().decode("ascii")
+
+        sys.stdout = IOWrapper(sys.stdout)
+
+    if dfs:
+        from types import GeneratorType
+
+        def bootstrap(f, stk=[]):
+            def wrappedfunc(*args, **kwargs):
+                if stk:
+                    return f(*args, **kwargs)
+                else:
+                    to = f(*args, **kwargs)
+                    while True:
+                        if type(to) is GeneratorType:
+                            stk.append(to)
+                            to = next(to)
+                        else:
+                            stk.pop()
+                            if not stk:
+                                break
+                            to = stk[-1].send(to)
+                    return to
+            return wrappedfunc
+
+    if hashing:
+        RANDOM = random.getrandbits(20)
+        class Wrapper(int):
+            def __init__(self, x):
+                int.__init__(x)
+
+            def __hash__(self):
+                return super(Wrapper, self).__hash__() ^ RANDOM
+
+    if read_from_file:
+        file = open("input.txt", "r").readline().strip()[1:-1]
+        fin = open(file, 'r')
+        input = lambda: fin.readline().strip()
+        output_file = open("output.txt", "w")
+        def fprint(*args, **kwargs):
+            print(*args, **kwargs, file=output_file)
+
+    if de:
+        def debug(*args, **kwargs):
+            # print('\033[92m', end='')
+            print(*args, **kwargs)
+            # print('\033[0m', end='')
+    else:
+        def debug(*args, **kwargs):
+            pass
+
+
+    fmax = lambda x, y: x if x > y else y
+    fmin = lambda x, y: x if x < y else y
+
+    class lst_lst:
+        def __init__(self, n) -> None:
+            self.n = n
+            self.pre = []
+            self.cur = []
+            self.notest = [-1] * (n + 1)
+
+        def append(self, i, j):
+            self.pre.append(self.notest[i])
+            self.notest[i] = len(self.cur)
+            self.cur.append(j)
+
+        def iterate(self, i):
+            tmp = self.notest[i]
+            while tmp != -1:
+                yield self.cur[tmp]
+                tmp = self.pre[tmp]
+
+# Can tell us the max frequency in the data structure in O(1)
+class MaxFrequencyChecker:
+    def __init__(self, arr):
+        self.frqs = [0] * (max(arr) + 1) # frqs[number] = frequency of that number
+        self.frqToNumsWithFrq = [0] * (len(arr) + 1) # frequency -> how many numbers have that frequency
+        for i, v in enumerate(arr):
+            self.frqs[v] += 1
+        for number, freq in enumerate(self.frqs):
+            self.frqToNumsWithFrq[freq] += 1
+        self.maxFrq = max(self.frqs)
+
+    # Amortized removal as each number can be removed only once
+    def removeNumber(self, number):
+        oldFrq = self.frqs[number]
+        newFrq = oldFrq - 1
+        self.frqToNumsWithFrq[oldFrq] -= 1
+        self.frqToNumsWithFrq[newFrq] += 1
+
+        self.frqs[number] -= 1
+
+        while self.frqToNumsWithFrq[self.maxFrq] == 0:
+            self.maxFrq -= 1
+
+    def getMaxFrq(self):
+        return self.maxFrq
+
+
+def solveTest(arr):
+    # some subequence of the form outerNum outerNum outerNum ... X X X X ... outerNum outerNum outerNum
+    def maximumLengthForOuterNumber(outerNum):
+        maxFrqChecker = MaxFrequencyChecker(arr)
+        l = 0
+        r = len(arr) - 1
+        outerNumsSeen = 0
+        result = 0
+
+        while l < r:
+            # if the left number isn't our outer number, we don't need to consider it
+            if arr[l] != outerNum:
+                maxFrqChecker.removeNumber(arr[l])
+                l += 1
+                continue
+            if arr[r] != outerNum:
+                maxFrqChecker.removeNumber(arr[r])
+                r -= 1
+                continue
+
+            # now the l-th and r-th numbers are the outer number, we need to find the maximum frequency in l+1...r-1
+            maxFrqChecker.removeNumber(arr[l])
+            maxFrqChecker.removeNumber(arr[r])
+            outerNumsSeen += 2
+            maxInner = maxFrqChecker.getMaxFrq()
+            # debug(f'outer: {outerNumsSeen}, inner: {maxInner}')
+            resultHere = outerNumsSeen + maxInner
+            result = fmax(result, resultHere)
+
+            l += 1
+            r -= 1
+
+        return result
+    uniqueNums = list(set(arr))
+    return max(1, max(maximumLengthForOuterNumber(outerNum) for outerNum in uniqueNums))
+
+t = II()
+for _ in range(t):
+  n = II()
+  arr = LII()
+  print(solveTest(arr))
